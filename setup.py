1ae2e1cbe1a8ff8f81c450968a3ac1c8c1ea4fdb
youkaichao <youkaichao@gmail.com>
2025-01-04 10:19:00 +0800

    fix linkage
    Signed-off-by: youkaichao <youkaichao@gmail.com>

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ module = Extension(
     sources=["vllm_allocator_adaptor_c.cpp"],
     include_dirs=[cuda_include_dir],  # Only your CUDA path, Python is auto-detected
     extra_compile_args=["-fPIC"],
+    libraries=["cuda"],  # Link against the libcuda library
     # Tell setuptools we want an abi3 wheel (for CPython >=3.8):
     py_limited_api=True,
     define_macros=[("Py_LIMITED_API", "0x03080000")],
